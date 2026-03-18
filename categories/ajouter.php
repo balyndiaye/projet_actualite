fc37e3fe63b1fc0480e89387279930b2266f54ca
diarra167 <dsall5967@gmail.com>
2026-03-18 12:56:23 +0000

    Dev C : Correction des droits d'accès (Lecture pour tous, Ajout pour Admin)

--- categories/ajouter.php
+++ categories/ajouter.php
@@ -1,24 +1,23 @@
 <?php 
+$role_requis = 'administrateur'; 
+include '../config/auth_check.php';
 require_once '../config/db.php';
 include '../entete.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $libelle = $_POST['libelle'];
     if (!empty($libelle)) {
-        // Requête préparée pour la sécurité
         $stmt = $pdo->prepare("INSERT INTO categories (libelle) VALUES (?)");
         $stmt->execute([$libelle]);
-        header("Location: liste.php");
+        header("Location: listes.php");
         exit();
     }
 }
 ?>
 <div class="container">
-    <h2>Ajouter une Catégorie</h2>
+    <h2>Nouvelle Catégorie</h2>
     <form method="POST">
-        <label>Nom de la catégorie :</label><br>
-        <input type="text" name="libelle" required><br><br>
+        <input type="text" name="libelle" placeholder="Nom (ex: Sport)" required>
         <button type="submit" class="btn">Enregistrer</button>
-        <a href="liste.php">Annuler</a>
     </form>
 </div>
